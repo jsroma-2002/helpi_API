05ab81a2d876beb5de4a63999d385fda32bd5d4c
jsroma <joseluis.rome@protonmail.com>
2022-05-16 00:25:21 -0500

    Initial Commit

---
+++ src/main/java/com/telegaming/helpi/HelpiApplication.java
@@ -0,0 +1,13 @@
+package com.telegaming.helpi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelpiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelpiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/telegaming/helpi/HelpiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.telegaming.helpi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelpiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
