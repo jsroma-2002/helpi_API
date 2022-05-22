760f43d9f63712826f3d65e178d10c54633687b4
Augusto Morante <augustomorante@hotmail.com>
2022-05-22 02:06:28 -0500

    update

--- src/main/java/com/telegaming/helpi/domain/service/CoachService.java
+++ src/main/java/com/telegaming/helpi/domain/service/CoachService.java
@@ -1,4 +1,16 @@
 package com.telegaming.helpi.domain.service;
 
+import com.telegaming.helpi.domain.model.Coach;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
+
 public interface CoachService {
+    Page<Coach> getAllCoaches(Pageable pageable);
+    Coach getCoachById(Long coachId);
+    Coach createCoach(Coach coach);
+    Coach updateCoach(Long coachId, Coach coachRequest);
+    Coach postTrainingMaterial(Long coachId, Long trainingId);
+    ResponseEntity<?> deleteCoach(Long coachId);
 }
--- src/main/java/com/telegaming/helpi/resource/coach/CoachResource.java
+++ src/main/java/com/telegaming/helpi/resource/coach/CoachResource.java
@@ -1,4 +1,77 @@
 package com.telegaming.helpi.resource.coach;
 
+import java.time.LocalDate;
+
 public class CoachResource {
+
+    private Long id;
+    private String name;
+    private String email;
+    private String password;
+    private String field;
+    private LocalDate birthDate;
+    private Integer age;
+
+    public Long getId() {
+        return id;
+    }
+
+    public CoachResource setId(Long id) {
+        this.id = id;
+        return this;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public CoachResource setName(String name) {
+        this.name = name;
+        return this;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public CoachResource setEmail(String email) {
+        this.email = email;
+        return this;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public CoachResource setPassword(String password) {
+        this.password = password;
+        return this;
+    }
+
+    public String getField() {
+        return field;
+    }
+
+    public CoachResource setField(String field) {
+        this.field = field;
+        return this;
+    }
+
+    public LocalDate getBirthDate() {
+        return birthDate;
+    }
+
+    public CoachResource setBirthDate(LocalDate birthDate) {
+        this.birthDate = birthDate;
+        return this;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public CoachResource setAge(Integer age) {
+        this.age = age;
+        return this;
+    }
 }
