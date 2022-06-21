4d9a5f8e1dc9e0647e147b7f71fafdfef092fe2e
Jose Roncal <48871991+jsroma-2002@users.noreply.github.com>
2022-06-21 16:07:50 -0500

    Update CoachServiceImplTest.java

--- src/test/java/com/telegaming/helpi/CoachServiceImplTest.java
+++ src/test/java/com/telegaming/helpi/CoachServiceImplTest.java
@@ -14,7 +14,7 @@ import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
-import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.boot.test.context.SpringBootTest;
 
 import java.util.Optional;
 
